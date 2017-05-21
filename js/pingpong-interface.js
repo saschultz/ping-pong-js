2f18ae2f678e3cd555ff1671b7fff095c37d9859
Sara Schultz <saschultz95@gmail.com>
2017-05-20 17:49:01 -0700

    create new file in js for the front end logic

---
+++ js/pingpong-interface.js
@@ -0,0 +1,11 @@
+//Front-End
+$(document).ready(function() {
+  $('#ping-pong-form').submit(function(event) {
+    event.preventDefault();
+    var goal = $('#goal').val();
+    var output = pingPong(goal);
+    output.forEach(function(element) {
+      $('#solution').append("<li>" + element + "</li>" );
+    });
+  })//end submit function
+});//end doc ready
