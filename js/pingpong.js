a211144a10d7d43738af58010bf62647efff5320
Sara Schultz <saschultz95@gmail.com>
2017-05-20 17:39:18 -0700

    add frontend logic in pingong.js

--- js/pingpong.js
+++ js/pingpong.js
@@ -16,3 +16,13 @@ function pingPong(goal) {
 }
 
 //Front-End
+$(document).ready(function() {
+  $('#ping-pong-form').submit(function(event) {
+    event.preventDefault();
+    var goal = $('#goal').val();
+    var output = pingPong(goal);
+    output.forEach(function(element) {
+      $('#solution').append("<li>" + element + "</li>" );
+    });
+  })//end submit function
+});//end doc ready
