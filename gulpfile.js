dc09833c4e7474855adb9eadb82b10d2656ea2f8
Nicole Santamaria and Sara Schultz <sara@>
2017-05-22 09:16:46 -0700

    reorder require statements in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,11 @@
 var gulp = require('gulp');
-var utilities = require('gulp-util');
-var del = require('del');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
+var utilities = require('gulp-util');
+var del = require('del');
 var jshint = require('gulp-jshint');
-
 var buildProduction = utilities.env.production;
 
 gulp.task('jsBrowserify', ['concatInterface'], function() {
