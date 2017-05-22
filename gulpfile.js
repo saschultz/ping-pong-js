5110b74e9102c5ae51ded4482cee1d5830605e81
Sara Schultz <saschultz95@gmail.com>
2017-05-21 20:41:54 -0700

    add browserify and vinyl-source-stream in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 var gulp = require('gulp');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
 
 gulp.task('myTask', function() {
   console.log('hello gulp');
+  // I am the inner anonymous function that is a parameter of gulp.task()!
 });
